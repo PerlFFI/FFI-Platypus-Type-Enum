cbf1a2d081f2f11779f4a0ec2bcc2647308c6456
Eugen Lut <misslsv@mail.ru>
2020-06-12 13:43:16 +0300

    Allow to deffer subroutine/constant calling

--- lib/FFI/Platypus/Type/Enum.pm
+++ lib/FFI/Platypus/Type/Enum.pm
@@ -202,6 +202,9 @@ sub ffi_custom_type_api_1
   foreach my $value (@values)
   {
     my $name;
+    if( ref $value eq 'CODE' ) {
+      $value =  $value->();
+    }
     if(is_plain_arrayref $value)
     {
       ($name,$index) = @$value;
